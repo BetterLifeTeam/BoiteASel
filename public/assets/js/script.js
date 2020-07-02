83ac48de3dd73fe683be33477b48d4ac705c6948
Laurie-03 <56717358+Laurie-03@users.noreply.github.com>
2020-07-02 21:43:01 +0200

    Bootstrap table export

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,3 +4,33 @@ jQuery(".duty_description").each(function () {
 });
 
 $("#conversation_content").animate({ scrollTop: $(this).height() }, "slow");
+
+$('#table_top_givers').bootstrapTable("destroy").bootstrapTable({
+   exportOptions: {
+      fileName: 'BoiteASel_Top_5_Depanneurs'
+   }
+});
+
+$('#table_top_askers').bootstrapTable("destroy").bootstrapTable({
+   exportOptions: {
+      fileName: 'BoiteASel_Top_5_Demandeurs'
+   }
+});
+
+$('#table_actuality').bootstrapTable("destroy").bootstrapTable({
+   exportOptions: {
+      fileName: 'BoiteASel_Actualite'
+   }
+});
+
+$('#table_activity_type').bootstrapTable("destroy").bootstrapTable({
+   exportOptions: {
+      fileName: 'BoiteASel_Types_Services'
+   }
+});
+
+$('#table_exchanges').bootstrapTable("destroy").bootstrapTable({
+   exportOptions: {
+      fileName: 'BoiteASel_Volume_Echanges'
+   }
+});
