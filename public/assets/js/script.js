0d59a8b73fbe609388f153bd6a0a366c5413727a
Laurie-03 <56717358+Laurie-03@users.noreply.github.com>
2020-07-02 21:12:42 +0200

    Corrections design

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,3 +2,5 @@ jQuery(".duty_description").each(function () {
    var description = $(".duty_description").text().substring(0,300);
    $(".duty_description").text(description+"...");
 });
+
+$("#conversation_content").animate({ scrollTop: $(this).height() }, "slow");
